461efbbe7163d3b44e0cd9e93e789cf622d5d1db
jailyn <aryanaaiken@gmail.com>
2025-12-07 21:43:30 -0500

    Main Menu
    Main menu scene and assets (without options scene)

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var audio_manager_scene = preload("res://scenes/audio_manager.tscn")
+#var audio_manager_scene = preload("res://scenes/audio_manager.tscn")
 
 func _ready():
 	#var audio_manager = audio_manager_scene.instantiate()
@@ -20,17 +20,18 @@ func _press_start():
 	# load game scene
 	#audio_manager_scene.play_sound()
 	#audioo.play_sound()
-	var game_scene = preload("res://scenes/TestLevel.tscn")
+	var game_scene = preload("res://scenes/Levels/MainLevel.tscn")
 	get_tree().change_scene_to_packed(game_scene)
 
 # Opttions button pressed
 func _press_options():
+	pass
 	# oad options scene
 	#audio_manager_scene.play_sound()
 	#button_sound.stream = preload("res://assets/ui/Pixel_32.wav")
 	#button_sound.play()
-	var options_scene = preload("res://scripts/systems/optionsscene.tscn")
-	get_tree().change_scene_to_packed(options_scene)
+	#var options_scene = preload("res://scripts/Levels/MainLevel.tscn")
+	#get_tree().change_scene_to_packed(options_scene)
 
 # Exit button pressed
 func _press_exit():
