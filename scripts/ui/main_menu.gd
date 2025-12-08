cbd8f81cd7b51249ef3c298083a5ef0ab44687aa
Safia Nassiri <niejelandrew@gmail.com>
2025-12-07 22:19:56 -0500

    Connected splash screen to main menu and that to main level scenes

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var audio_manager_scene = preload("res://scenes/audio_manager.tscn")
+var audio_manager_scene = preload("res://scenes/UI/main_menu.tscn")
 
 func _ready():
 	#var audio_manager = audio_manager_scene.instantiate()
@@ -9,10 +9,9 @@ func _ready():
 	
 	#var audioo = get_node("AudioManager/AudioStreamPlayer")
 	
-	
 	# s button signals
 	$VBoxContainer/StartButton.pressed.connect(_press_start)
-	$VBoxContainer/OptionsButton.pressed.connect(_press_options)
+	#$VBoxContainer/OptionsButton.pressed.connect(_press_options)
 	$VBoxContainer/ExitButton.pressed.connect(_press_exit)
 
 # Start button pressed
@@ -20,17 +19,17 @@ func _press_start():
 	# load game scene
 	#audio_manager_scene.play_sound()
 	#audioo.play_sound()
-	var game_scene = preload("res://scenes/TestLevel.tscn")
+	var game_scene = preload("res://scenes/Levels/MainLevel.tscn")
 	get_tree().change_scene_to_packed(game_scene)
 
 # Opttions button pressed
-func _press_options():
-	# oad options scene
-	#audio_manager_scene.play_sound()
-	#button_sound.stream = preload("res://assets/ui/Pixel_32.wav")
-	#button_sound.play()
-	var options_scene = preload("res://scripts/systems/optionsscene.tscn")
-	get_tree().change_scene_to_packed(options_scene)
+#func _press_options():
+	## oad options scene
+	##audio_manager_scene.play_sound()
+	##button_sound.stream = preload("res://assets/ui/Pixel_32.wav")
+	##button_sound.play()
+	#var options_scene = preload("res://scripts/systems/optionsscene.tscn")
+	#get_tree().change_scene_to_packed(options_scene)
 
 # Exit button pressed
 func _press_exit():
